b480dc4ce2ff2794f815f06346a881cc0abf2694
M511-49\admin <s3a432090@student.ncut.edu.tw>
2017-10-11 08:52:13 +0800

    增加任務

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,16 @@ Route::get('/', function () {
  * 增加新的任務
  */
 Route::post('/task', function (Request $request) {
-    //
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
 });
 
 /**
