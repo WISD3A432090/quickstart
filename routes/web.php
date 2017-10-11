e6343a5f30a47685e25223deabaa61935023d8b1
M511-49\admin <s3a432090@student.ncut.edu.tw>
2017-10-11 08:46:34 +0800

    init commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('task');
+});
+
+/**
+ * 增加新的任務
+ */
+Route::post('/task', function (Request $request) {
+    //
+});
+
+/**
+ * 刪除任務
+ */
+Route::delete('/task/{task}', function (Task $task) {
+    //
+});
